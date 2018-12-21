fa1eff0bdd99e991ccda9dfe72c7c63454b406d3
Sun <22406854+SunTheCourier@users.noreply.github.com>
2018-12-20 21:47:54 -0800

    Update Help.cs

--- Commands/Help.cs
+++ Commands/Help.cs
@@ -15,7 +15,7 @@ namespace Sombra_Bot.Commands
             EmbedBuilder builder = new EmbedBuilder();
             builder.WithTitle("Help Menu");
             builder.AddInlineField("Hack", "<level of hack, 1: kick 2: ban> <user> <reason>");
-            builder.WithFooter("All commands should start with `.s`");
+            builder.WithFooter("All commands should start with `s.`");
             builder.WithColor(Color.Purple);
 
             await Context.Channel.SendMessageAsync("Here are my commands:", embed: builder);
