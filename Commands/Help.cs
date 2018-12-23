661f35704ff40c1fe8938a6aa744c7892d94447a
Sun <22406854+SunTheCourier@users.noreply.github.com>
2018-12-22 17:56:41 -0800

    Add OWstats

--- Commands/Help.cs
+++ Commands/Help.cs
@@ -11,13 +11,17 @@ namespace Sombra_Bot.Commands
 
         public async Task Helpmsg()
         {
+            await Context.Channel.TriggerTypingAsync();
+            
             EmbedBuilder builder = new EmbedBuilder();
             builder.WithTitle("Help Menu");
-            builder.AddInlineField("GetRelease", "Gets a release from the specificed Github repository\n args: <repository owner> <repository name>");
-            builder.AddInlineField("Hack", "Kicks or Bans a user\n args: <level of hack, 1: kick, 2: ban> <user> <reason>");
+            builder.AddField("GetRelease", "Gets a release from the specificed Github repository\n args: <repository owner> <repository name>");
+            builder.AddField("OWStats", "Gets Overwatch stats\n args: <Battle.net tag>");
+            builder.AddField("Hack", "Kicks or Bans a user\n args: <level of hack, 1: kick, 2: ban> <user> <reason>");
             builder.WithFooter("All commands should start with `s.`");
             builder.WithColor(Color.Purple);
 
+            Task.Delay(500);
             await Context.Channel.SendMessageAsync("Here are my commands:", embed: builder);
         }
     }
---
+++ Commands/OWStats.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using OverwatchAPI;
+using Sombra_Bot.Utils;
+using System.Collections.Generic;
+
+namespace Sombra_Bot.Commands
+{
+    public class OWStats : ModuleBase<SocketCommandContext>
+    {
+        [Command("OWStats"), Summary("Gets your Overwatch stats!")]
+
+        public async Task GetstatsOW(string User)
+        {
+            await Context.Channel.TriggerTypingAsync();
+            using (var owClient = new OverwatchClient())
+            {
+                Player player;
+                try
+                {
+                    player = await owClient.GetPlayerAsync(User);
+                }
+                catch
+                {
+                    await Error.Send("User does not exist.", Context.Channel);
+                    return;
+                }
+                if (player.IsProfilePrivate) await Error.Send("Player profile is private", Context.Channel, "Change your profile to public");
+
+                EmbedBuilder builder = new EmbedBuilder();
+                builder.WithTitle($"{player.Username}'s Stats");
+                builder.WithUrl(player.ProfileUrl);
+                builder.WithColor(Color.Orange);
+                builder.WithTimestamp(DateTimeOffset.Now);
+                builder.WithThumbnailUrl(player.ProfilePortraitUrl);
+                builder.AddField("Player Level (Not Inlcuding Prestige(s))", player.PlayerLevel);
+                if (player.CompetitiveRank == 0) builder.AddField("Competetive Rank", "Not Placed");
+                else builder.AddField("Competetive Rank", $"SR: {player.CompetitiveRank}: {GetRankName(player.CompetitiveRank)}");
+                //`: {GetRankName(player.CompetitiveRank)}`
+                //builder.AddInlineField("Achievements", $"{player.Achievements.Count}/{player.Achievements.Capacity}"); bugged
+                string endorvalue = "";
+                foreach (KeyValuePair<Endorsement, decimal> s in player.Endorsements)
+                {
+                    //maybe multiply by 100?
+                    string endor = $"{s.Key}: {s.Value.ToString().Replace("0.", " ")}%, ";
+                    endorvalue += endor;
+                }
+                builder.AddField($"Endorsment Level: {player.EndorsementLevel}", endorvalue);
+                builder.AddField("Platform", player.Platform.ToString().ToUpper());
+
+                await Context.Channel.SendMessageAsync("", embed: builder);
+            }
+        }
+
+        private string GetRankName(int SR)
+        {
+
+            switch (SR)
+            {
+                case int n when n >= 1 && n <= 1499:
+                    return "Bronze";
+
+                case int n when n >= 1500 && n <= 1999:
+                    return "Silver";
+
+                case int n when n >= 2000 && n <= 2499:
+                    return "Gold";
+
+                case int n when n >= 2500 && n <= 2999:
+                    return "Platinum";
+
+                case int n when n >= 3000 && n <= 3499:
+                    return "Diamond";
+
+                case int n when n >= 3500 && n <= 3999:
+                    return "Master";
+
+                case int n when n >= 4000:
+                    return "Grandmaster";
+                default:
+                    return null;
+            }
+        }
+
+    }
+}
