88947ffcd881f8124ed094029a02f6e2a123b1c8
Sun <22406854+SunTheCourier@users.noreply.github.com>
2018-12-24 12:39:45 -0800

    Create Presence.cs

---
+++ Commands/Presence.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace Sombra_Bot.Commands
+{
+    public class Presence : ModuleBase<SocketCommandContext>
+    {
+        [Command("setpresence"), Summary("Sets the game presence")]
+        [RequireOwner]
+        public async Task SetPresence(params string[] input)
+        {
+            await Context.Client.SetGameAsync(string.Join(" ", input));
+            await Context.Channel.SendMessageAsync("Done!");
+        }
+
+        [Command("resetpresence"), Summary("Resets the game presence")]
+        [RequireOwner]
+        public async Task ResetPresence()
+        {
+            await Context.Client.SetGameAsync("Hacking the planet");
+            await Context.Channel.SendMessageAsync("Done!");
+        }
+    }
+}
