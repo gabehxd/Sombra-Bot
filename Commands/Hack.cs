f1b7430375ec4c24f63810ecd50166b66686c955
Sun <22406854+SunTheCourier@users.noreply.github.com>
2018-12-29 18:57:31 -0800

    Check Perms Separately

--- Commands/Hack.cs
+++ Commands/Hack.cs
@@ -9,10 +9,7 @@ namespace Sombra_Bot.Commands
     public class Hack : ModuleBase<SocketCommandContext>
     {
         [Command("Hack"), Summary("Bans or kicks a user.")]
-        //Is there a way to check if a user has one perm or the other? In the main function possibly?
-        [RequireUserPermission(GuildPermission.KickMembers)]
-        [RequireUserPermission(GuildPermission.BanMembers)]
-        public async Task ManageUser(int level, IUser user, string reason = null)
+        public async Task ManageUser(int level, IGuildUser user, string reason = null)
         {
             if (Context.User == user && level == 1)
             {
@@ -30,34 +27,43 @@ namespace Sombra_Bot.Commands
             {
                 case 1:
                     //no kick method?
-                    try
+                    if (user.GuildPermissions.KickMembers)
                     {
-                        await Context.Guild.AddBanAsync(user, reason: reason);
-                        await Context.Guild.RemoveBanAsync(user);
-                        await Context.Channel.SendMessageAsync(Getmessage());
-                        await Context.Channel.TriggerTypingAsync();
-                        await Context.Channel.SendMessageAsync($"{user} Hacked!");
+                        try
+                        {
+                            await Context.Guild.AddBanAsync(user, reason: reason);
+                            await Context.Guild.RemoveBanAsync(user);
+                            await Context.Channel.SendMessageAsync(Getmessage());
+                            await Context.Channel.TriggerTypingAsync();
+                            await Context.Channel.SendMessageAsync($"{user} Hacked!");
+                        }
+                        catch
+                        {
+                            await Error.Send(Context.Channel, Value: "User could not be kicked, insufficent permissions.");
+                        }
                     }
-                    catch
-                    {
-                        await Error.Send(Context.Channel, Value: "User could not be kicked, insufficent permissions.");
-                    }
+                    else await Error.Send(Context.Channel, Value: "User requires guild permission KickMembers");
+
                     break;
                 case 2:
-                    try
+                    if (user.GuildPermissions.BanMembers)
                     {
-                        await Context.Guild.AddBanAsync(user, reason: reason);
-                        await Context.Channel.SendMessageAsync(Getmessage());
-                        await Context.Channel.TriggerTypingAsync();
-                        await Context.Channel.SendMessageAsync($"{user} Hacked!");
+                        try
+                        {
+                            await Context.Guild.AddBanAsync(user, reason: reason);
+                            await Context.Channel.SendMessageAsync(Getmessage());
+                            await Context.Channel.TriggerTypingAsync();
+                            await Context.Channel.SendMessageAsync($"{user} Hacked!");
+                        }
+                        catch
+                        {
+                            await Error.Send(Context.Channel, Value: "User could not be banned, insufficent permissions.");
+                        }
                     }
-                    catch
-                    {
-                        await Error.Send(Context.Channel, Value: "User could not be banned, insufficent permissions.");
-                    }
+                    else await Error.Send(Context.Channel, Value: "User requires guild permission BanMembers");
                     break;
                 default:
-                    await Error.Send(Context.Channel, Value: "Inputted hack level does not exist");
+                    await Error.Send(Context.Channel, Key: "Inputted hack level does not exist");
                     break;
             }
         }
