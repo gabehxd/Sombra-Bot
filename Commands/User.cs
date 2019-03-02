7ebee5f3bfbcfd40bc57fc0f16756a7874d0ba29
Sun <22406854+SunTheCourier@users.noreply.github.com>
2019-03-02 12:11:06 -0800

    Avatar Command

---
+++ Commands/User.cs
@@ -0,0 +1,20 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+using Discord;
+
+namespace Sombra_Bot.Commands
+{
+    public class User : ModuleBase<SocketCommandContext>
+    {
+        [Command("Avatar"), Alias("Ava", "pfp"), Summary("Grabs a User Avatar")]
+        public async Task GetAvatar(IUser user)
+        {
+            EmbedBuilder builder = new EmbedBuilder();
+            builder.WithColor(Color.Purple);
+            builder.WithTitle($"{user.Username}'s Avatar");
+            //Is 2048 always the best size?
+            builder.WithImageUrl(user.GetAvatarUrl(ImageFormat.Auto, 2048));
+            await Context.Channel.SendMessageAsync(embed: builder.Build());
+        }
+    }
+}
