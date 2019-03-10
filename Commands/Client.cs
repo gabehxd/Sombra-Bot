8e73484d3f0d86f3498f14c6873e31206d4a5194
Sun <22406854+SunTheCourier@users.noreply.github.com>
2019-03-10 00:26:06 -0800

    Move Invite and change link

--- Commands/Client.cs
+++ Commands/Client.cs
@@ -13,5 +13,11 @@ namespace Sombra_Bot.Commands
             await Context.Channel.SendMessageAsync("Bye bitch.");
             Environment.Exit(0);
         }
+
+        [Command("Invite"), Summary("Get an invite.")]
+        public async Task GetInvite()
+        {
+            await Context.Channel.SendMessageAsync("Invite link: https://discordbots.org/bot/516009170353258496\nDiscord server: https://discord.gg/jQ8HuWE\nSource code: https://github.com/SunTheCourier/Sombra-Bot");
+        }
     }
 }
