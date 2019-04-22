7621a1f2627f065e649c358651981df7bb2ea81b
Sun <22406854+SunTheCourier@users.noreply.github.com>
2019-04-22 14:51:53 -0700

    Update BotBan.cs

--- Commands/BotBan.cs
+++ Commands/BotBan.cs
@@ -9,38 +9,36 @@ namespace Sombra_Bot.Commands
     {
         [Command("AddBotBan"), Summary("Ban a user from this bot.")]
         [RequireOwner]
-        public async Task AddBan(ulong ID)
+        public async Task AddBan(IGuildUser user)
         {
-            //better search function?
-            foreach (ulong user in Save.BannedUsers.Data)
+            if (Save.BannedUsers.Data.Contains(user.Id))
             {
-                if (user == ID)
-                {
-                    await Error.Send(Context.Channel, Value: "User is already banned.");
-                    return;
-                }
+                await Error.Send(Context.Channel, Value: "User is already banned.");
+                return;
             }
-            Save.BannedUsers.Data.Add(ID);
+            Save.BannedUsers.Data.Add(user.Id);
             await Context.Channel.SendMessageAsync("User added to ban list.");
         }
 
+
         [Command("RemoveBotBan"), Summary("Removes banned user from this bot.")]
         [RequireOwner]
-        public async Task RemoveBan(ulong ID)
+        public async Task RemoveBan(IGuildUser user)
         {
             if (Save.BannedUsers.Data.Count != 0)
             {
-                if (Save.BannedUsers.Data.Remove(ID))
+                if (Save.BannedUsers.Data.Remove(user.Id))
                 {
                     await Context.Channel.SendMessageAsync("User removed from ban list.");
                     return;
                 }
                 await Error.Send(Context.Channel, Value: "No user with that ID is banned.");
+                return;
             }
             await Error.Send(Context.Channel, Value: "No users are banned.");
         }
 
-        [Command("ListBotBans"), Summary("Removes banned user from this bot."), Alias("Bans")]
+        [Command("ListBotBans"), Summary("Lists all banned users."), Alias("Bans")]
         [RequireOwner]
         public async Task ListBans()
         {
