95f8583d6ef5ba623f930014a35fcfce1da40591
Sun <22406854+SunTheCourier@users.noreply.github.com>
2019-05-20 22:29:57 -0700

    Add nick and update help

--- Commands/Help.cs
+++ Commands/Help.cs
@@ -9,31 +9,32 @@ namespace Sombra_Bot.Commands
     public class Help : ModuleBase<SocketCommandContext>
     {
 
-        private Dictionary<string, string> commands = new Dictionary<string, string>
+        private readonly Dictionary<string, string> commands = new Dictionary<string, string>
         {
-            { "Help", "DMs you the help menu, arguments are optional.\n args: <command>." },
+            { "Help", "DMs you the help menu\n args: [command]." },
             { "ListRoles",  "Lists all roles in the current server." },
             { "AddRole", "Adds a role to the specified user, requires manage roles permission.\nargs: <user> <role>." },
             { "RemoveRole", "Removes a role from the specified user, requires manage roles permission.\nargs: <user> <role>." },
-            { "Hack", "Kicks or bans a user, requires ban/kick permissions.\nargs: <level of hack, 1: kick, 2: ban> <user> <reason>." },
+            { "Hack", "Kicks or bans a user, requires ban/kick permissions.\nargs: <level of hack, 1: kick, 2: ban> <user> [reason]." },
             { "Hacc", "Haccs a user >:3.\nargs: <user>"},
             { "GetRelease", "Gets a release from the specificed Github repository.\nargs: <repository owner> <repository name>." },
-            { "Roll", "Rolls a max of 100 die with a max of 100 sides.\nargs: <die> <sides>." },
+            { "Roll", "Rolls a max of 100 die with a max of 100 sides.\nargs: [die = 2] [sides = 6]." },
             { "Flip", "Flips a coin."},
-            { "Avatar", "Gets a user's Avatar.\nargs: <user>." },
+            { "Avatar", "Gets a user's Avatar.\nargs: [user = self]." },
+            { "Nick", "Changes your or another user's nickname.\nargs: <nickname> [user = self]."},
             { "OWStats", "Gets Overwatch stats.\nargs: <username>." },
             { "Ping", "Pings a IP address.\nargs: <IP>"},
             { "EnableMemes", "Re-enables random memes, requires the manage server permission. Enabled by default." },
             { "DisableMemes", "Disables random memes from being sent into the server, requires the manage server permission."},
-            { "Invite", "Gets an invite for Sombra Bot and Sombra Bot's discord server." }
+            { "Invite", "Gets an invite for Sombra Bot and Sombra Bot's discord server." },
+#if PUBLIC
+            { "Suggest", $"Suggest a feature for {Program.AppInfo.Owner.Mention} to add. Do not spam this command or you will be banned from using this bot :).\nargs: <suggestion>"}
+#endif
         };
 
         [Command("Help"), Summary("Get help.")]
         public async Task Helpmsg(string command = null)
         {
-#if PUBLIC
-            commands.Add("Suggest", $"Suggest a feature for {Program.AppInfo.Owner.Mention} to add. Do not spam this command or you will be banned from using this bot :).\nargs: <suggestion>");
-#endif
             //Can the help message be automated?
             EmbedBuilder builder = new EmbedBuilder();
             builder.WithTitle("Help Menu");
--- Commands/User.cs
+++ Commands/User.cs
@@ -1,6 +1,8 @@
 ﻿using Discord.Commands;
 using System.Threading.Tasks;
 using Discord;
+using Discord.WebSocket;
+using Sombra_Bot.Utils;
 
 namespace Sombra_Bot.Commands
 {
@@ -15,6 +17,47 @@ namespace Sombra_Bot.Commands
             builder.WithTitle($"{user.Username}'s Avatar");
             //Is 2048 always the best size?
             builder.WithImageUrl(user.GetAvatarUrl(ImageFormat.Auto, 2048));
+            await Context.Channel.SendMessageAsync(embed: builder.Build());
+        }
+
+        [Command("Nick"), Summary("Nicknames a user"), RequireBotPermission(GuildPermission.ManageNicknames)]
+        public async Task SetNick(string nick, SocketGuildUser user = null)
+        {
+            IGuildUser contextuser = Context.User as IGuildUser;
+            if (contextuser == null)
+            {
+                if (contextuser.GuildPermissions.ChangeNickname) user = Context.User as SocketGuildUser;
+                else 
+                {
+                    await Error.Send(Context.Channel, Value: "User requires guild permission ChangeNickname.");
+                    return;
+                }
+            }
+            else if (user != Context.User as SocketGuildUser && !contextuser.GuildPermissions.ManageNicknames)
+            {
+                await Error.Send(Context.Channel, Value: "User requires guild permission ManageNicknames.");
+                return;
+            }
+            else if (user == Context.User as SocketGuildUser && contextuser.GuildPermissions.ChangeNickname)
+            {
+                await Error.Send(Context.Channel, Value: "User requires guild permission ChangeNickname.");
+                    return;
+            }
+
+
+            try
+            {
+                await user.ModifyAsync(x => { x.Nickname = nick; });
+            }
+            catch
+            {
+                await Error.Send(Context.Channel, Value: "Not enough permissions");
+                return;
+            }
+            EmbedBuilder builder = new EmbedBuilder();
+            builder.WithColor(Color.Green);
+            builder.WithTitle("Success");
+            builder.WithDescription($"{user.Mention}'s Nickname has been set!");
             await Context.Channel.SendMessageAsync(embed: builder.Build());
         }
     }
